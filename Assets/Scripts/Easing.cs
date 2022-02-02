f154af069b24a718324e0f257efff515d208b9d3
Cathan Bertram <55544660+CathanBertram@users.noreply.github.com>
2022-02-02 16:21:55 +0000

    Final easing functions

--- Assets/Scripts/Easing.cs
+++ Assets/Scripts/Easing.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum EasingType
@@ -16,7 +18,22 @@ public enum EasingType
     EaseInOutQuart,
     EaseInQuint,
     EaseOutQuint,
-    EaseInOutQuint
+    EaseInOutQuint,
+    EaseInSine,
+    EaseOutSine,
+    EaseInOutSine, 
+    EaseInExpo,
+    EaseOutExpo,
+    EaseInOutExpo,
+    EaseInCirc,
+    EaseOutCirc,
+    EaseInOutCirc,
+    EaseInBack,
+    EaseOutBack,
+    EaseInOutBack, 
+    EaseInBounce,
+    EaseOutBounce,
+    EaseInOutBounce
 }
 
 public static class Easing
@@ -86,6 +103,122 @@ public static class Easing
     public static float EaseInOutQuint(float a, float b, float t)
     {
         t = t < 0.5f ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
+        return a + (b - a) * t;
+    }
+
+    public static float EaseInSine(float a, float b, float t)
+    {
+        return a + (b - a) * (1 - Mathf.Cos((t * Mathf.PI) * 0.5f));
+    }
+    public static float EaseOutSine(float a, float b, float t)
+    {
+        return a + (b - a) * (Mathf.Sin((t * Mathf.PI) * 0.5f));
+    }
+    public static float EaseInOutSine(float a, float b, float t)
+    {
+        return a + (b - a) * (-(Mathf.Cos(Mathf.PI * t) - 1) * 0.5f);
+    }
+
+    public static float EaseInExpo(float a, float b, float t)
+    {
+        t = t == 0 ? 0 : Mathf.Pow(2, 10 * t - 10);
+        return a + (b - a) * t;
+    }
+
+    public static float EaseOutExpo(float a, float b, float t)
+    {
+        t = t == 1 ? 1 : 1 - Mathf.Pow(2, -10 * t);
+        return a + (b - a) * t;
+    }
+
+    public static float EaseInOutExpo(float a, float b, float t)
+    {
+        t = t == 0 ? 0 :
+            t == 1 ? 1 :
+            t < 0.5f ? Mathf.Pow(2, 20 * t - 10) * 0.5f : 
+            (2 - Mathf.Pow(2, -20 * t + 10)) * 0.5f;
+        return a + (b - a) * t;
+    }
+
+    public static float EaseInCirc(float a, float b, float t)
+    {
+        return a + (b - a) * (1 - Mathf.Sqrt(1 - t * t));
+    }
+
+    public static float EaseOutCirc(float a, float b, float t)
+    {
+        return a + (b - a) * (Mathf.Sqrt(1 - (t - 1) * (t - 1)));
+    }
+
+    public static float EaseInOutCirc(float a, float b, float t)
+    {
+        t = t < 0.5f
+            ? (1 - Mathf.Sqrt(1 - (2 * t) * (2 * t))) * 0.5f
+            : (Mathf.Sqrt(1 - (-2 * t + 2) * (-2 * t + 2)) + 1) * 0.5f;
+
+        return a + (b - a) * t;
+    }
+
+    public static float EaseInBack(float a, float b, float t)
+    {
+        const float c1 = 1.70158f;
+        const float c3 = c1 + 1;
+
+        return a + (b - a) * (c3 * t * t * t - c1 * t * t);
+    }
+
+    public static float EaseOutBack(float a, float b, float t)
+    {
+        const float c1 = 1.70158f;
+        const float c3 = c1 + 1;
+
+        return a + (b - a) * (1 + c3 * ((t - 1) * (t - 1) * (t - 1)) + c1 * ((t - 1) * (t - 1)));
+    }
+
+    public static float EaseInOutBack(float a, float b, float t)
+    {
+        const float c1 = 1.70158f;
+        const float c2 = c1 * 1.525f;
+
+        t = t < 0.5f
+            ? ((2 * t) * (2 * t) * ((c2 + 1) * 2 * t - c2)) * 0.5f
+            : ((2 * t - 2) * (2 * t - 2) * ((c2 + 1) * (t * 2 - 2) + c2) + 2) * 0.5f;
+
+        return a + (b - a) * t;
+    }
+
+    public static float EaseInBounce(float a, float b, float t)
+    {
+        return a + (b - a) * (1 - EaseOutBounce(1 - t));
+    }
+
+    public static float EaseOutBounce(float a, float b, float t)
+    {
+        return a + (b - a) * EaseOutBounce(t);
+    }
+    
+    public static float EaseOutBounce(float t)
+    {
+        const float n1 = 7.5625f;
+        const float d1 = 2.75f;
+
+        if (t < 1 / d1) {
+            return n1 * t * t;
+        } else if (t < 2 / d1) {
+            return n1 * (t -= 1.5f / d1) * t + 0.75f;
+        } else if (t < 2.5 / d1) {
+            return n1 * (t -= 2.25f / d1) * t + 0.9375f;
+        } else {
+            return n1 * (t -= 2.625f / d1) * t + 0.984375f;
+        }
+    }
+
+    public static float EaseInOutBounce(float a, float b, float t)
+    {
+        t = t < 0.5f
+            ? (1 - EaseOutBounce(1 - 2 * t)) * 0.5f
+            : (1 + EaseOutBounce(2 * t - 1)) * 0.5f;
+
         return a + (b - a) * t;
     }
 }
--- Assets/Scripts/Tests/EasingTest.cs
+++ Assets/Scripts/Tests/EasingTest.cs
@@ -54,66 +54,63 @@ namespace Tests
 
             for (int x = 0; x < size; x++)
             {
-                int y = 0;
-                switch (easeType)
+                int y = easeType switch
                 {
-                    case EasingType.Linear:
-                        y = Mathf.RoundToInt(Easing.Linear(0, size, x / (float)size));
-                        break;
-                    case EasingType.EaseInQuadratic:
-                        y = Mathf.RoundToInt(Easing.EaseInQuadratic(0, size, x / (float)size));
-                        break;
-                    case EasingType.EaseOutQuadratic:
-                        y = Mathf.RoundToInt(Easing.EaseOutQuadratic(0, size, x / (float)size));
-                        break;
-                    case EasingType.EaseInOutQuadratic:
-                        y = Mathf.RoundToInt(Easing.EaseInOutQuadratic(0, size, x / (float)size));
-                        break;
-                    case EasingType.EaseInCubic:
-                        y = Mathf.RoundToInt(Easing.EaseInCubic(0, size, x / (float)size));
-                        break;
-                    case EasingType.EaseOutCubic:
-                        y = Mathf.RoundToInt(Easing.EaseOutCubic(0, size, x / (float)size));
-                        break;
-                    case EasingType.EaseInOutCubic:
-                        y = Mathf.RoundToInt(Easing.EaseInOutCubic(0, size, x / (float)size));
-                        break;
-                    case EasingType.EaseInQuart:
-                        y = Mathf.RoundToInt(Easing.EaseInQuart(0, size, x / (float)size));
-                        break;
-                    case EasingType.EaseOutQuart:
-                        y = Mathf.RoundToInt(Easing.EaseOutQuart(0, size, x / (float)size));
-                        break;
-                    case EasingType.EaseInOutQuart:
-                        y = Mathf.RoundToInt(Easing.EaseInOutQuart(0, size, x / (float)size));
-                        break;
-                    case EasingType.EaseInQuint:
-                        y = Mathf.RoundToInt(Easing.EaseInQuint(0, size, x / (float)size));
-                        break;
-                    case EasingType.EaseOutQuint:
-                        y = Mathf.RoundToInt(Easing.EaseOutQuint(0, size, x / (float)size));
-                        break;
-                    case EasingType.EaseInOutQuint:
-                        y = Mathf.RoundToInt(Easing.EaseInOutQuint(0, size, x / (float)size));
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-                
-                SetColours(x, y);
+                    EasingType.Linear => Mathf.RoundToInt(Easing.Linear(0, size, x / (float) size)),
+                    EasingType.EaseInQuadratic => Mathf.RoundToInt(Easing.EaseInQuadratic(0, size, x / (float) size)),
+                    EasingType.EaseOutQuadratic => Mathf.RoundToInt(Easing.EaseOutQuadratic(0, size, x / (float) size)),
+                    EasingType.EaseInOutQuadratic => Mathf.RoundToInt(Easing.EaseInOutQuadratic(0, size, x / (float) size)),
+                    EasingType.EaseInCubic => Mathf.RoundToInt(Easing.EaseInCubic(0, size, x / (float) size)),
+                    EasingType.EaseOutCubic => Mathf.RoundToInt(Easing.EaseOutCubic(0, size, x / (float) size)),
+                    EasingType.EaseInOutCubic => Mathf.RoundToInt(Easing.EaseInOutCubic(0, size, x / (float) size)),
+                    EasingType.EaseInQuart => Mathf.RoundToInt(Easing.EaseInQuart(0, size, x / (float) size)),
+                    EasingType.EaseOutQuart => Mathf.RoundToInt(Easing.EaseOutQuart(0, size, x / (float) size)),
+                    EasingType.EaseInOutQuart => Mathf.RoundToInt(Easing.EaseInOutQuart(0, size, x / (float) size)),
+                    EasingType.EaseInQuint => Mathf.RoundToInt(Easing.EaseInQuint(0, size, x / (float) size)),
+                    EasingType.EaseOutQuint => Mathf.RoundToInt(Easing.EaseOutQuint(0, size, x / (float) size)),
+                    EasingType.EaseInOutQuint => Mathf.RoundToInt(Easing.EaseInOutQuint(0, size, x / (float) size)),
+                    EasingType.EaseInSine => Mathf.RoundToInt(Easing.EaseInSine(0, size, x / (float) size)),
+                    EasingType.EaseOutSine => Mathf.RoundToInt(Easing.EaseOutSine(0, size, x / (float) size)),
+                    EasingType.EaseInOutSine => Mathf.RoundToInt(Easing.EaseInOutSine(0, size, x / (float) size)),
+                    EasingType.EaseInExpo => Mathf.RoundToInt(Easing.EaseInExpo(0, size, x / (float) size)),
+                    EasingType.EaseOutExpo => Mathf.RoundToInt(Easing.EaseOutExpo(0, size, x / (float) size)),
+                    EasingType.EaseInOutExpo => Mathf.RoundToInt(Easing.EaseInOutExpo(0, size, x / (float) size)),
+                    EasingType.EaseInCirc => Mathf.RoundToInt(Easing.EaseInCirc(0, size, x / (float) size)),
+                    EasingType.EaseOutCirc => Mathf.RoundToInt(Easing.EaseOutCirc(0, size, x / (float) size)),
+                    EasingType.EaseInOutCirc => Mathf.RoundToInt(Easing.EaseInOutCirc(0, size, x / (float) size)),
+                    EasingType.EaseInBack => Mathf.RoundToInt(Easing.EaseInBack(0, size, x / (float) size)),
+                    EasingType.EaseOutBack => Mathf.RoundToInt(Easing.EaseOutBack(0, size, x / (float) size)),
+                    EasingType.EaseInOutBack => Mathf.RoundToInt(Easing.EaseInOutBack(0, size, x / (float) size)),
+                    EasingType.EaseInBounce => Mathf.RoundToInt(Easing.EaseInBounce(0, size, x / (float) size)),
+                    EasingType.EaseOutBounce => Mathf.RoundToInt(Easing.EaseOutBounce(0, size, x / (float) size)),
+                    EasingType.EaseInOutBounce => Mathf.RoundToInt(Easing.EaseInOutBounce(0, size, x / (float) size)),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+
+                // if (y < size * 0.5f)
+                // {
+                //     y += 20;
+                // }else if (y > size * 0.5f)
+                // {
+                //     y -= 20;
+                // }
+
+                SetColours(x, y, Color.black);
             }
             
+            SetColours(0,0, Color.red);
+
             texture.Apply();
         }
-        private void SetColours(int x, int y)
+        private void SetColours(int x, int y, Color color)
         {
-            texture.SetPixel(x, y, Color.black);
+            texture.SetPixel(x, y, color);
 
             for (int x1 = x - lineWidth; x1 < x + lineWidth; x1++)
             {
                 for (int y1 = y - lineWidth; y1 < y + lineWidth; y1++)
                 {
-                    texture.SetPixel(x1, y1, Color.black);
+                    texture.SetPixel(x1, y1, color);
                 }
             }
         }
